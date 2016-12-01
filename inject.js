d65e9eb181b4125acf18d10ff6708fc6b0b8d36a
g.paradiso <g.paradiso@accenture.com>
2016-12-01 09:08:47 +0100

    Fake accepted answer handling added

--- inject.js
+++ inject.js
@@ -10,25 +10,37 @@ let checkGrayUrl = chrome.extension.getURL('imgs/check_gray.svg');
 let checkMarkElement = $("<img src='" + checkMarkUrl + "' title=\"This is the top voted answer and should be accepted\" \\>");
 let checkGrayElement = $("<img src='" + checkGrayUrl + "' title=\"This is the top voted answer and should be accepted\" \\>");
 
-let acceptedAnswer = $(acceptedAnswerClass);
-let acceptedAnswerId = $(acceptedAnswer)[0].id;
+let answers = $(answerClass);
 
-let topVotedAnswer = _.maxBy($(answerClass), o => {
-    let numberRaw = $(o).find(voteCountClass).text();
-    return parseInt(numberRaw);
-});
+if (answers.length > 0) {
 
-let topVotedAnswerId = $(topVotedAnswer)[0].id;
+    // Searching for top voted answer
+    let topVotedAnswer = _.maxBy($(answerClass), o => {
+        let numberRaw = $(o).find(voteCountClass).text();
+        return parseInt(numberRaw);
+    });
 
-/*
-    console.log("Is max answer accepted?");
-    console.log(topVotedAnswerId === acceptedAnswerId);
-    console.log("topVotedAnswer id: " + topVotedAnswerId);
-    console.log("acceptedAnswer id: " + acceptedAnswerId);
-*/
+    let topVotedAnswerId = $(topVotedAnswer)[0].id;
 
-if (topVotedAnswerId !== acceptedAnswerId) {
-    $(topVotedAnswer).find(voteDivClass).append(checkMarkElement);
-    $(acceptedAnswer).find(voteCheckmarkClass).remove();
-    $(acceptedAnswer).find(voteDivClass).append(checkGrayElement);
+    // Searching for accepted answer
+    let acceptedAnswer = $(acceptedAnswerClass);
+
+    if (acceptedAnswer.length > 0) {
+
+        // If accepted answer found: Checking if it's a wrong accepted answer
+        let acceptedAnswerId = $(acceptedAnswer)[0].id;
+
+        if (topVotedAnswerId !== acceptedAnswerId) {
+            $(topVotedAnswer).find(voteDivClass).append(checkMarkElement);
+            $(acceptedAnswer).find(voteCheckmarkClass).remove();
+            $(acceptedAnswer).find(voteDivClass).append(checkGrayElement);
+        }
+    }
+    else {
+
+        // If no accepted answer found: Checking if it has votes
+        if ($(topVotedAnswer).find(voteCountClass).text() > 0) {
+            $(topVotedAnswer).find(voteDivClass).append(checkMarkElement);
+        }
+    }
 }
